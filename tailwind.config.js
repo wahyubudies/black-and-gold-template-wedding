97b799aeaa0309e602282bd29fb7d422de9ad50d
wahyubudies <wahyubudies1@gmail.com>
2022-01-19 22:38:59 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'orange-level-1': "#E1A170",
-        'orange-level-2': "#F6E0CF",
-        'gray-level-1': "#E0E0E0",
-        'gray-level-2': "#F3F3F3",
-
         'gray-black-gold': "#373737",
         'black-black-gold': '#1f1f1f',
         'gold-black-gold-level-1': '#DBC087',
